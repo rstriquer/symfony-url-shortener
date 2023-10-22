6fedbcca3f0daed728d0f21298cf2868b09aae9f
rstriquer <rstriquer@gmail.com>
2023-10-22 19:24:58 -0300

    feat: Adds Entity URL Repository and Service
    - Adds Entities and traits support files

---
+++ src/Repository/UrlRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Url;
+use App\Trait\EntityManagerFlushTrait;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Illuminate\Support\Str;
+
+/**
+ * @extends ServiceEntityRepository<Url>
+ *
+ * @method Url|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Url|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Url[]    findAll()
+ * @method Url[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UrlRepository extends ServiceEntityRepository
+{
+    use EntityManagerFlushTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Url::class);
+    }
+
+    public function getNewUniqueTag(string $tag = null) : string
+    {
+        if ($tag === null) {
+            $tag = Str::random(6);
+        }
+
+        if ($this->findOneBy([ 'tag' => $tag ])) {
+            return $this->getNewUniqueTag();
+        }
+
+        return $tag;
+    }
+
+//    /**
+//     * @return Url[] Returns an array of Url objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('u.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Url
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
---
+++ src/Trait/EntityManagerFlushTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Trait;
+
+trait EntityManagerFlushTrait
+{
+    /**
+     * Flush an entity into the Entity Manager
+     * @param $entity A Doctrine entity object
+     * @throws \Exception Could throw any kind of Doctrine available exception
+     */
+    public function flush($entity)
+    {
+        $this->_em->persist($entity);
+        $this->_em->flush();
+    }
+}
